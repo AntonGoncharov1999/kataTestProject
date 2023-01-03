62f4837d946f8f251973df4f92b4fe8685f78092
Anton Goncharov <42060678+AntonGoncharov1999@users.noreply.github.com>
2023-01-03 03:06:38 +0300

    3 итерация
    доработал дизайн и добавил регулярные выражения

--- Script/script.js
+++ Script/script.js
@@ -7,10 +7,12 @@ calc.addEventListener('click', function (event) {
     let value = (event.target.innerText);
 
     switch (value) {
-        case 'Отчистка':
+        case 'C':
             result.innerText = ''
             break
-        case 'Результат':
+        case 'Enter':
+            if (result.innerText.search(/[^0-9*/+-.]/mi) != -1) return;
+
             result.innerText = eval(result.innerText).toFixed(2)
             break
         default:
