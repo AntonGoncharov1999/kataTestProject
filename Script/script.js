4b1e8b1e5958f0365ce21dfdff8903e89b450854
Anton Goncharov <42060678+AntonGoncharov1999@users.noreply.github.com>
2023-01-03 21:20:54 +0300

    4 итерация
    доработал обработчик ввода для калькулятора

--- Script/script.js
+++ Script/script.js
@@ -6,17 +6,38 @@ calc.addEventListener('click', function (event) {
 
     let value = (event.target.innerText);
 
+    //ассеты регулярных выражений для проверок коректности ввода
+    let asset = /[^0-9*/+-.]/mi;
+    let asset2 = /\+{2,}/gi;
+    let asset3 = /\-{2,}/gi;
+    let asset4 = /\*{2,}/gi;
+    let asset5 = /\/{2,}/gi;
+    let asset6 = /\.{2,}/gi;
+
+
     switch (value) {
         case 'C':
             result.innerText = ''
             break
         case 'Enter':
-            if (result.innerText.search(/[^0-9*/+-.]/mi) != -1) return;
-            result.innerText = eval(result.innerText).toFixed(2)
+            if (result.innerText.search(asset) != -1) return;
+
+            if (asset2.test(result.innerText)) {
+                result.innerText = eval(result.innerText.replace(asset2, "+")).toFixed(2)
+            } else if (asset3.test(result.innerText)) {
+                result.innerText = eval(result.innerText.replace(asset3, "-")).toFixed(2)
+            } else if (asset4.test(result.innerText)) {
+                result.innerText = eval(result.innerText.replace(asset4, "*")).toFixed(2)
+            } else if (asset5.test(result.innerText)) {
+                result.innerText = eval(result.innerText.replace(asset5, "/")).toFixed(2)
+            } else if (asset6.test(result.innerText)) {
+                result.innerText = eval(result.innerText.replace(asset6, ".")).toFixed(2)
+            } else {
+                result.innerText = eval(result.innerText).toFixed(2)
+            }
             break
         default:
             result.innerText += value;
 
     }
-
 });
